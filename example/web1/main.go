a38e59e3a2b84c01f4fbd09d9d3530d3ad1ecc16
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2020-05-04 13:42:57 +0000

    Fix bugs and support multiple services
    Signed-off-by: Mohammed Aboullaite <aboullaite.mohammed@gmail.com>

--- example/web1/main.go
+++ example/web1/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -20,10 +21,15 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+	for {
+		time.Sleep(2 * time.Second)
+		go log.Println("** Service Started on Port " + port + " **")
+
+	}
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
-	io.WriteString(w, `{"status":"ok ferp"}`)
+	io.WriteString(w, `{"status":"toto"}`)
 
 }
---
+++ example/web2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	http.HandleFunc("/", ExampleHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("** Service Started on Port " + port + " **")
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	for {
+		time.Sleep(2 * time.Second)
+		go log.Println("** Service Started on Port " + port + " **")
+
+	}
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, `{"status":"nok"}`)
+
+}
